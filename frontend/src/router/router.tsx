9ad1707e7c09313a1c6665cc0786b98a100dd0fa
shiningKyung <nkyou15@gmail.com>
2023-11-28 21:18:30 +0900

    Fix: CRA에서 Vite으로 마이그레이션

---
+++ frontend/src/router/router.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+
+const Router = () => {
+    return (
+        <Routes>
+			{/* <Route path='*' element={<Error />} /> */}
+			<Route />
+		</Routes>
+    );
+};
+
+export default Router;
