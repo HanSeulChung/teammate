7f8c77ffb108dcee169730fe8d689acae8d3fbad
zzannorita <tpals0518@naver.com>
2023-12-03 15:15:34 +0900

    Add: SignInStyled.tsx, 입력 유효성 검사, 비밀번호 보안 강화 추가

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import Calender from '../views/Calender';
+import SignIn from '../components/SignIn';
+import SignUp from '../components/SignUp';
 
 const Router = () => {
     return (
         <Routes>
-			{/* <Route path='*' element={<Error />} /> */}
+			<Route path='/로그인' element={<SignIn />} />
+			<Route path='/회원가입' element={<SignUp />} />
 			<Route path='/캘린더' element={<Calender />} />
-			{/* <Route /> */}
+			<Route />
 		</Routes>
     );
 };
