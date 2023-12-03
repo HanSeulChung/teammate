33c95674e67f5d64ac15090ccc6093dd6e6bda87
bslkuye <jdh960401@naver.com>
2023-12-03 20:45:31 +0900

    Add: prettier, eslint적용, 테스트코드 제거

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter } from "react-router-dom"
-import Router from './router/router'
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./router/router";
 
 function App() {
-    const [count, setCount] = useState(0)
-
-    return (
-        <BrowserRouter>
-            {/* <Navbar /> */}
-            <section>
-                <Router />
-            </section>
-            {/* <Footer /> */}
-        </BrowserRouter>
-        
-    )
+  return (
+    <BrowserRouter>
+      {/* <Navbar /> */}
+      <section>
+        <Router />
+      </section>
+      {/* <Footer /> */}
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
