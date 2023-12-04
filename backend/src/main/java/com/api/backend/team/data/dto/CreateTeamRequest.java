ee361541309b3f4803320b1b7bf55d2612a02f22
jin young lee <dkwktm45@naver.com>
2023-12-04 11:33:55 +0900

    Add: Request 객체 Valid 추가

--- backend/src/main/java/com/api/backend/team/data/dto/CreateTeamRequest.java
+++ backend/src/main/java/com/api/backend/team/data/dto/CreateTeamRequest.java
@@ -1,5 +1,6 @@
 package com.api.backend.team.data.dto;
 
+import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,10 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class CreateTeamRequest {
+  @NotBlank(message = "팀 이름이 존재하지 않습니다.")
   private String teamName;
+  @NotBlank(message = "팀 이미지가 존재하지 않습니다.")
   private String teamImg;
+  @NotBlank(message = "팀 제한이 설정되어 있지 않습니다.")
   private int memberLimit;
 }
