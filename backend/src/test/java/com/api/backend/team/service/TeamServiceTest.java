e59ac8f2b407c81764f7532645a045b5f0719e10
jin young lee <dkwktm45@naver.com>
2023-12-04 14:24:06 +0900

    Test: 검증 로직 추가에 따른 Test로직 수정 및 추가 (#28)

--- backend/src/test/java/com/api/backend/team/service/TeamServiceTest.java
+++ backend/src/test/java/com/api/backend/team/service/TeamServiceTest.java
@@ -1,10 +1,12 @@
 package com.api.backend.team.service;
 
+import static com.api.backend.global.exception.type.ErrorCode.TEAM_PARTICIPANTS_NOT_VALID_EXCEPTION;
 import static org.assertj.core.api.BDDAssumptions.given;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
+import com.api.backend.global.exception.CustomException;
 import com.api.backend.team.data.entity.Team;
 import com.api.backend.team.data.repository.TeamParticipantsRepository;
 import com.api.backend.team.data.repository.TeamRepository;
@@ -32,8 +34,8 @@ class TeamServiceTest {
   }
 
   @Test
-  @DisplayName("팀 초대 코드 url 로직")
-  void getTeamUrl() {
+  @DisplayName("팀 초대 코드 url 로직 - [성공]")
+  void getTeamUrl_success() {
     //given
     Long id = 1L;
     String userId = "1";
@@ -43,10 +45,35 @@ class TeamServiceTest {
         .build();
     when(teamRepository.findById(anyLong()))
         .thenReturn(Optional.of(team));
+    when(teamParticipantsRepository.existsByIdAndMember_MemberId(id, Long.valueOf(userId)))
+        .thenReturn(true);
     //when
     String result = teamService.getTeamUrl(id,userId);
 
     //then
     assertEquals(result,team.getInviteLink());
+  }
+
+  @Test
+  @DisplayName("팀 초대 코드 url 로직 - [실패] 팀원이 아닐경우")
+  void getTeamUrl_fail() {
+    //given
+    Long id = 1L;
+    String userId = "1";
+    Team team = Team.builder()
+        .teamId(1L)
+        .profileUrl("2/dsfefsefnklsd")
+        .build();
+    when(teamRepository.findById(anyLong()))
+        .thenReturn(Optional.of(team));
+    when(teamParticipantsRepository.existsByIdAndMember_MemberId(id, Long.valueOf(userId)))
+        .thenReturn(false);
+    //when
+    CustomException result = assertThrows(CustomException.class,
+        () -> teamService.getTeamUrl(id,userId));
+
+    //then
+    assertEquals(result.getErrorMessage(),TEAM_PARTICIPANTS_NOT_VALID_EXCEPTION.getErrorMessage());
+    assertEquals(result.getErrorCode().getCode(),500);
   }
 }
