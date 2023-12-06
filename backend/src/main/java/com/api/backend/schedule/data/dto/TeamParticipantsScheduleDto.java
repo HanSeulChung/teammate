406a56a3424032a067b9286d60033a38be700d39
zenxxn <bunbun2438@naver.com>
2023-12-06 12:45:31 +0900

    Add: 팀참가자일정 DTO 생성

---
+++ backend/src/main/java/com/api/backend/schedule/data/dto/TeamParticipantsScheduleDto.java
@@ -0,0 +1,38 @@
+package com.api.backend.schedule.data.dto;
+
+import com.api.backend.schedule.data.enetity.TeamParticipantsSchedule;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TeamParticipantsScheduleDto {
+  private Long teamParticipantsScheduleId;
+  private Long scheduleId;
+  private Long teamParticipantsId;
+  public static TeamParticipantsScheduleDto of(TeamParticipantsSchedule teamParticipantsSchedule) {
+    return TeamParticipantsScheduleDto.builder()
+        .teamParticipantsScheduleId(teamParticipantsSchedule.getTeamParticipantsScheduleId())
+        .scheduleId(teamParticipantsSchedule.getSchedule().getScheduleId())
+        .teamParticipantsId(teamParticipantsSchedule.getTeamParticipants().getTeamParticipantsId())
+        .build();
+  }
+
+  public static List<TeamParticipantsScheduleDto> of(Page<TeamParticipantsSchedule> teamParticipantsSchedules) {
+    if (teamParticipantsSchedules != null) {
+      List<TeamParticipantsScheduleDto> teamParticipantsScheduleDtoList = new ArrayList<>();
+      for (TeamParticipantsSchedule teamParticipantsSchedule : teamParticipantsSchedules) {
+        teamParticipantsScheduleDtoList.add(TeamParticipantsScheduleDto.of(teamParticipantsSchedule));
+      }
+      return teamParticipantsScheduleDtoList;
+    }
+    return new ArrayList<>();
+  }
+}
