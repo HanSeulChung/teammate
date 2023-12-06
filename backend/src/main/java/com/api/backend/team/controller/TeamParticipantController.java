f9dbcee2556e663e66520f3aaeb9baef5a0a1618
jin young lee <dkwktm45@naver.com>
2023-12-06 15:47:03 +0900

    Add: TeamParticipantController RequestMapping 추가

--- backend/src/main/java/com/api/backend/team/controller/TeamParticipantController.java
+++ backend/src/main/java/com/api/backend/team/controller/TeamParticipantController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/team/{teamId}/participant")
 public class TeamParticipantController {
 
   private final TeamParticipantsService teamParticipantsService;
