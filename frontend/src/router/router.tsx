f26a89a85ff9f4e52a832dfc72e61575a276c82b
정도헌 <jdh960401@naver.com>
2023-12-06 20:08:30 +0900

    Test : 웹소켓 연결 테스트코드

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -3,13 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import Calender from '../views/Calender';
 import SignIn from '../components/SignIn';
 import SignUp from '../components/SignUp';
+import TextEditorView from '../views/TextEditorView';
+import MyComponent from '../components/WebSocketTest';
 
 const Router = () => {
     return (
         <Routes>
 			{/* <Route path='*' element={<Error />} /> */}
 			<Route path='/캘린더' element={<Calender />} />
-			<Route />
+			<Route path='text-editor' element={<TextEditorView/>}/>
+			<Route path='test' element={<MyComponent />} />
 		</Routes>
     );
 };
