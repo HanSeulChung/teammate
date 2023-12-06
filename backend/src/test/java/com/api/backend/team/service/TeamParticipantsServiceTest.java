470c7ec125252cca475f83db119c35e823632c38
jin young lee <dkwktm45@naver.com>
2023-12-06 14:43:43 +0900

    Test: 팀원에게 역할 부여 service 로직 (#46)

--- backend/src/test/java/com/api/backend/team/service/TeamParticipantsServiceTest.java
+++ backend/src/test/java/com/api/backend/team/service/TeamParticipantsServiceTest.java
@@ -1,12 +1,14 @@
 package com.api.backend.team.service;
 
 import static com.api.backend.team.data.ResponseMessage.DELETE_TEAM_PARTICIPANT;
+import static com.api.backend.team.data.ResponseMessage.UPDATE_ROLE_TEAM_PARTICIPANT;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
+import com.api.backend.team.data.entity.Team;
 import com.api.backend.team.data.entity.TeamParticipants;
 import com.api.backend.team.data.repository.TeamParticipantsRepository;
 import com.api.backend.team.data.type.TeamRole;
@@ -44,5 +46,37 @@ class TeamParticipantsServiceTest {
 
     //then
     assertEquals(result,DELETE_TEAM_PARTICIPANT);
+  }
+
+  @Test
+  @DisplayName("팀장이 팀원에게 권한부여 로직")
+  void updateRoleTeamParticipant(){
+    //given
+    String userId = "1";
+    Long participantId = 1L;
+    Long teamId = 1L;
+    Team team = Team.builder().teamId(1L).build();
+    TeamParticipants teamParticipants = TeamParticipants.builder()
+        .teamRole(TeamRole.READER)
+        .team(team)
+        .build();
+    TeamParticipants teamParticipants2 = TeamParticipants.builder()
+        .teamRole(TeamRole.MATE)
+        .team(team)
+        .build();
+
+    when(teamParticipantsRepository.findByTeam_TeamIdAndMember_MemberId(
+        anyLong(),
+        anyLong()
+    )).thenReturn(Optional.of(teamParticipants));
+    when(teamParticipantsRepository.findById(anyLong()))
+        .thenReturn(Optional.of(teamParticipants2));
+
+    //when
+    String result = teamParticipantsService.updateRoleTeamParticipant(userId, participantId,
+        teamId);
+
+    //then
+    assertEquals(result,UPDATE_ROLE_TEAM_PARTICIPANT);
   }
 }
