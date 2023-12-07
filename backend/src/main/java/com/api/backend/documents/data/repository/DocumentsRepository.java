fd2da30112bb9ecc30ae21f3ac5b56efa9d31485
HanSeul <w0w1278@naver.com>
2023-12-07 13:18:33 +0900

    Feat: 문서 전체 조회 기능 (#42)
    현재 Entity -> Response(Dto)로 변환하는 과정에서 id가 null로 가져오게 됨.
    추후 수정 필요

--- backend/src/main/java/com/api/backend/documents/data/repository/DocumentsRepository.java
+++ backend/src/main/java/com/api/backend/documents/data/repository/DocumentsRepository.java
@@ -1,10 +1,13 @@
 package com.api.backend.documents.data.repository;
 
 import com.api.backend.documents.data.entity.Documents;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DocumentsRepository extends JpaRepository<Documents,Long> {
-
+public interface DocumentsRepository extends MongoRepository<Documents, Long> {
+  @Override
+  Page<Documents> findAll(Pageable pageable);
 }
