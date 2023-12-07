18022c01d45880633565ca89eb9613db6f4e5957
HanSeul <w0w1278@naver.com>
2023-12-07 20:57:33 +0900

    Add: JpaAuditing Repositories에 TeamParticipantsRepository추가

--- backend/src/main/java/com/api/backend/global/config/JpaAuditingConfig.java
+++ backend/src/main/java/com/api/backend/global/config/JpaAuditingConfig.java
@@ -4,6 +4,7 @@ import com.api.backend.category.data.repository.ScheduleCategoryRepository;
 import com.api.backend.member.data.repository.MemberRepository;
 import com.api.backend.notification.data.repository.NotificationRepository;
 import com.api.backend.schedule.data.repository.ScheduleRepository;
+import com.api.backend.team.data.repository.TeamParticipantsRepository;
 import com.api.backend.team.data.repository.TeamRepository;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -12,7 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration
 @EnableJpaAuditing
 @EnableJpaRepositories(basePackageClasses = {MemberRepository.class, ScheduleRepository.class,
-            NotificationRepository.class, TeamRepository.class, ScheduleCategoryRepository.class})
+    TeamParticipantsRepository.class, NotificationRepository.class, TeamRepository.class, ScheduleCategoryRepository.class})
 public class JpaAuditingConfig {
 
 }
