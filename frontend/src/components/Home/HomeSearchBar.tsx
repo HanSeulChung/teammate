43ee4904815f1675282d9cf3158050c2789ce498
zzannorita <tpals0518@naver.com>
2023-12-07 18:39:13 +0900

    Feat: 홈 페이지 서치바 구현

--- frontend/src/components/Home/HomeSearchBar.tsx
+++ frontend/src/components/Home/HomeSearchBar.tsx
@@ -1,24 +1,41 @@
 import React from "react";
 import { useRecoilState } from "recoil";
-import { searchState } from "../../state/authState";
+import {
+  searchState,
+  teamListState,
+  useSearchState,
+} from "../../state/authState";
 
 export default function HomeSearchBar() {
-  const [searchTerm, setSearchTerm] = useRecoilState(searchState);
+  const { search, setSearch, handleSearch } = useSearchState();
+  // const [searchTeam, setSearchTeam] = useRecoilState(searchState);
+  // const setTeamList = useRecoilState(teamListState)[1];
 
-  const handleSearch = () => {
-    // 검색을 수행하거나 다른 작업을 수행할 수 있습니다.
-    console.log("검색어:", searchTerm);
-  };
+  // const handleSearch = async () => {
+  //   // 검색을 수행하거나 다른 작업을 수행할 수 있습니다.
+  //   console.log("검색어:", searchTeam);
+
+  //   const searchResults = await fetchTeamsBySearchTeam(searchTeam);
+  //   setTeamList(searchResults);
+  // };
+
+  // // 실제 서버와 통신하여 검색 결과를 가져오는 함수
+  // const fetchTeamsBySearchTeam = async (searchTeam: string) => {
+  //   const response = await fetch(`/api/teams?search=${searchTeam}`);
+  //   const data = await response.json();
+
+  //   return data;
+  // };
 
   return (
     <div>
       <input
         type="text"
         placeholder="검색어를 입력하세요"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
-      <button onClick={handleSearch}>검색</button>
+      <button onClick={() => handleSearch(search)}>검색</button>
     </div>
   );
 }
--- frontend/src/state/authState.tsx
+++ frontend/src/state/authState.tsx
@@ -1,9 +1,12 @@
+// authState.ts
+
 import { atom, useRecoilState } from "recoil";
+import { useEffect } from "react";
 
 // 로그인 상태를 저장하는 atom
 export const isAuthenticatedState = atom({
   key: "isAuthenticatedState",
-  default: Boolean(localStorage.getItem("accessToken")), // 로컬 스토리지에 accessToken이 있는지 여부에 따라 초기값 설정
+  default: Boolean(localStorage.getItem("accessToken")),
 });
 
 // accessToken을 저장하는 함수
@@ -20,21 +23,52 @@ export const logout = () => {
   localStorage.removeItem("accessToken");
 };
 
-//홈화면 검색창
+// 홈화면 검색창
 export const searchState = atom({
   key: "searchState",
-  default: "",
+  default: (() => {
+    // 로컬 스토리지에서 검색어 가져오기
+    const storedSearch = localStorage.getItem("search");
+    return storedSearch || "";
+  })() as string,
 });
 
-//팀 생성창
+export const useSearchState = () => {
+  const [search, setSearch] = useRecoilState(searchState);
+  const [teamList, setTeamList] = useRecoilState(teamListState);
+  const handleSearch = async (searchTerm: string) => {
+    console.log("검색어:", searchTerm);
+
+    const searchResults = await fetchTeamsBySearchTeam(searchTerm);
+    setTeamList(searchResults);
+  };
+
+  const fetchTeamsBySearchTeam = async (searchTeam: string) => {
+    const response = await fetch(`/api/teams?search=${searchTeam}`);
+    const data = await response.json();
+    return data;
+  };
+
+  return { search, setSearch, handleSearch, teamList, setTeamList };
+};
+
+// 팀 생성창
 export const teamNameState = atom({
   key: "teamNameState",
-  default: "",
+  default: () => {
+    // 로컬 스토리지에서 팀 이름 가져오기
+    const storedTeamName = localStorage.getItem("teamName");
+    return storedTeamName || "";
+  },
 });
 
 export const selectedTeamSizeState = atom({
   key: "selectedTeamSizeState",
-  default: "1-9",
+  default: () => {
+    // 로컬 스토리지에서 선택된 팀 크기 가져오기
+    const storedTeamSize = localStorage.getItem("selectedTeamSize");
+    return storedTeamSize || "1-9";
+  },
 });
 
 export interface Team {
@@ -45,19 +79,37 @@ export interface Team {
 
 export const teamListState = atom<Team[]>({
   key: "teamListState",
-  default: [],
+  default: [], // 초기값을 빈 배열로 설정
+
+  // Recoil 초기화 함수 사용
+  effects_UNSTABLE: [
+    ({ setSelf }) => {
+      // 로컬 스토리지에서 팀 목록 가져오기
+      const storedTeamList = localStorage.getItem("teamList");
+      if (storedTeamList) {
+        setSelf(JSON.parse(storedTeamList));
+      }
+    },
+  ],
 });
 
-//마이페이지 (사용자 정보)
+// 마이페이지 (사용자 정보)
 export interface User {
   id: string;
   name: string;
-  // password: string;
 }
 
 export const userState = atom<User | null>({
   key: "userState",
-  default: null,
+  default: null, // 직접 값을 지정
+  effects_UNSTABLE: [
+    ({ onSet }) => {
+      // 아톰 값이 변경될 때 로컬 스토리지에 사용자 정보 저장
+      onSet((newValue) => {
+        localStorage.setItem("user", JSON.stringify(newValue));
+      });
+    },
+  ],
 });
 
 export const useUser = () => {
@@ -68,6 +120,14 @@ export const useUser = () => {
     // 사용자 정보를 localStorage에 저장
     localStorage.setItem("user", JSON.stringify(loggedInUser));
   };
+
+  useEffect(() => {
+    // 앱이 로드될 때 사용자 정보를 localStorage에서 복구
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, [setUser]);
 
   return { user, setUser, saveUser };
 };
