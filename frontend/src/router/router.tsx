3df3a93cdd171fb50156084e402012a9518efbaa
정도헌 <jdh960401@naver.com>
2023-12-08 12:47:26 +0900

    Add : 테스트 파일 삭제

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -4,8 +4,6 @@ import Calender from "../views/Calender";
 import SignIn from "../components/SignIn";
 import SignUp from "../components/SignUp";
 import TextEditorView from "../views/TextEditorView";
-import MyComponent from "../components/WebSocketTest";
-import TextEditorViewV2 from "../views/TextEditorViewV2";
 
 const Router = () => {
   return (
@@ -13,8 +11,6 @@ const Router = () => {
       {/* <Route path='*' element={<Error />} /> */}
       <Route path="/캘린더" element={<Calender />} />
       <Route path="text-editor" element={<TextEditorView />} />
-      <Route path="text-editorV2" element={<TextEditorViewV2 />} />
-      <Route path="test" element={<TextEditorViewV2 />} />
       <Route path="test" element={<MyComponent />} />
     </Routes>
   );
