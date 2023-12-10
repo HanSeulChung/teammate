941074140683d1b3a9212e2b320c2e359efca3e4
HanSeul <w0w1278@naver.com>
2023-12-11 00:29:31 +0900

    Add: Swagger 문서 comment dto 부분 추가

--- backend/src/main/java/com/api/backend/comment/data/dto/CommentEditRequest.java
+++ backend/src/main/java/com/api/backend/comment/data/dto/CommentEditRequest.java
@@ -1,5 +1,7 @@
 package com.api.backend.comment.data.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +12,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommentEditRequest {
+  @NotNull
+  @Schema(description = "editor id", example = "12L")
   private Long editorId;
+
+  @NotNull
+  @Schema(description = "comment title", example = "해당 사안 수정했습니다. 확인 부탁 드려요.")
   private String content;
 }
--- backend/src/main/java/com/api/backend/comment/data/dto/DeleteCommentsResponse.java
+++ backend/src/main/java/com/api/backend/comment/data/dto/DeleteCommentsResponse.java
@@ -1,5 +1,7 @@
 package com.api.backend.comment.data.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,11 +11,21 @@ import lombok.Getter;
 @AllArgsConstructor
 public class DeleteCommentsResponse {
 
+
+  @NotNull
+  @Schema(description = "deleted comment id", example = "6575d6fe99101a62f9710877")
   private String id;
 
+
+  @NotNull
+  @Schema(description = "deleted writer id", example = "12L")
   private Long writerId;
 
+  @NotNull
+  @Schema(description = "deleted comment title", example = "해당 사안 확인했습니다.")
   private String content;
 
+  @NotNull
+  @Schema(description = "deleted comment message", example = "삭제 되었습니다.")
   private String message;
 }
