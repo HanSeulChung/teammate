0b6a4a779b5076b486d877917aa37e33ff7613b3
HanSeul <w0w1278@naver.com>
2023-12-11 00:29:19 +0900

    Add: Swagger 문서 comment controller 부분 추가

--- backend/src/main/java/com/api/backend/comment/controller/CommentController.java
+++ backend/src/main/java/com/api/backend/comment/controller/CommentController.java
@@ -6,6 +6,12 @@ import com.api.backend.comment.data.dto.CommentResponse;
 import com.api.backend.comment.data.dto.DeleteCommentsResponse;
 import com.api.backend.comment.data.entity.Comment;
 import com.api.backend.comment.service.CommentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import java.security.Principal;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,18 +26,55 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
+@Api(tags = "댓글")
 @RequiredArgsConstructor
 @RequestMapping("/team/{teamId}/documents/{documentId}/comments")
 public class CommentController {
 
   private final CommentService commentService;
 
+  @ApiOperation(value = "해당 문서의 댓글을 생성합니다.")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "해당 문서의 댓글을 생성했습니다."),
+      @ApiResponse(code = 404, message = "페이지를 찾을 수 없습니다."),
+      @ApiResponse(code = 400, message = "CustomException을 반환합니다."),
+  })
+  @ApiImplicitParams(
+      {
+          @ApiImplicitParam(
+              name = "access token"
+              , value = "jwt access token"
+              , required = true
+              , dataType = "String"
+              , paramType = "header"
+              , defaultValue = "None"
+          ),
+          @ApiImplicitParam(
+              name = "teamId"
+              , value = "팀 id"
+              , required = true
+              , dataType = "Long"
+              , paramType = "path"
+              , defaultValue = "None"
+              , example = "1"),
+
+          @ApiImplicitParam(
+              name = "documentId"
+              , value = "문서 id"
+              , required = true
+              , dataType = "String"
+              , paramType = "path"
+              , defaultValue = "None"
+              , example = "657595c6c97b622e0440f394")
+      })
   @PostMapping()
   public ResponseEntity<CommentResponse> createComments(
       @PathVariable Long teamId, @PathVariable String documentId,
       @RequestBody @Valid CommentInitRequest request,
+      @ApiIgnore
       Principal principal
   ) {
     Comment comment = commentService.createComment(teamId, documentId, request, principal);
@@ -39,9 +82,44 @@ public class CommentController {
     return ResponseEntity.ok(CommentResponse.from(comment));
   }
 
+  @ApiOperation(value = "해당 문서의 댓글을 조회합니다.")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "해당 문서의 전체 댓글을 가져왔습니다."),
+      @ApiResponse(code = 404, message = "페이지를 찾을 수 없습니다."),
+      @ApiResponse(code = 400, message = "CustomException을 반환합니다."),
+  })
+  @ApiImplicitParams(
+      {
+          @ApiImplicitParam(
+              name = "access token"
+              , value = "jwt access token"
+              , required = true
+              , dataType = "String"
+              , paramType = "header"
+              , defaultValue = "None"
+          ),
+          @ApiImplicitParam(
+              name = "teamId"
+              , value = "팀 id"
+              , required = true
+              , dataType = "Long"
+              , paramType = "path"
+              , defaultValue = "None"
+              , example = "1"),
+
+          @ApiImplicitParam(
+              name = "documentId"
+              , value = "문서 id"
+              , required = true
+              , dataType = "String"
+              , paramType = "path"
+              , defaultValue = "None"
+              , example = "657595c6c97b622e0440f394")
+      })
   @GetMapping()
   public ResponseEntity<Page<CommentResponse>> getCommentsList(
       @PathVariable Long teamId, @PathVariable String documentId,
+      @ApiIgnore
       Principal principal,
       Pageable pageable) {
     Page<Comment> commentPage = commentService.getCommentList(teamId, documentId, principal, pageable);
@@ -52,11 +130,54 @@ public class CommentController {
     return ResponseEntity.ok(commentDtoPage);
   }
 
+  @ApiOperation(value = "해당 문서의 댓글을 수정합니다.")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "해당 문서의 댓글을 수정했습니다."),
+      @ApiResponse(code = 404, message = "페이지를 찾을 수 없습니다."),
+      @ApiResponse(code = 400, message = "CustomException을 반환합니다."),
+  })
+  @ApiImplicitParams(
+      {
+          @ApiImplicitParam(
+              name = "access token"
+              , value = "jwt access token"
+              , required = true
+              , dataType = "String"
+              , paramType = "header"
+              , defaultValue = "None"
+          ),
+          @ApiImplicitParam(
+              name = "teamId"
+              , value = "팀 id"
+              , required = true
+              , dataType = "Long"
+              , paramType = "path"
+              , defaultValue = "None"
+              , example = "1"),
+
+          @ApiImplicitParam(
+              name = "documentId"
+              , value = "문서 id"
+              , required = true
+              , dataType = "String"
+              , paramType = "path"
+              , defaultValue = "None"
+              , example = "657595c6c97b622e0440f394"),
+          @ApiImplicitParam(
+              name = "commentId"
+              , value = "댓글 id"
+              , required = true
+              , dataType = "String"
+              , paramType = "path"
+              , defaultValue = "None"
+              , example = "6575d6fe99101a62f9710877")
+      })
   @PutMapping("/{commentId}")
   public ResponseEntity<CommentResponse> editComment(
       @PathVariable Long teamId, @PathVariable String documentId,
       @PathVariable String commentId,
       @RequestBody @Valid CommentEditRequest request,
+      @ApiIgnore
       Principal principal
   ) {
     Comment comment = commentService.editComment(teamId, documentId, commentId, request, principal);
@@ -64,6 +185,48 @@ public class CommentController {
     return ResponseEntity.ok(CommentResponse.from(comment));
   }
 
+  @ApiOperation(value = "해당 문서의 댓글을 삭제합니다.")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "해당 문서의 댓글을 삭제했습니다."),
+      @ApiResponse(code = 404, message = "페이지를 찾을 수 없습니다."),
+      @ApiResponse(code = 400, message = "CustomException을 반환합니다."),
+  })
+  @ApiImplicitParams(
+      {
+          @ApiImplicitParam(
+              name = "access token"
+              , value = "jwt access token"
+              , required = true
+              , dataType = "String"
+              , paramType = "header"
+              , defaultValue = "None"
+          ),
+          @ApiImplicitParam(
+              name = "teamId"
+              , value = "팀 id"
+              , required = true
+              , dataType = "Long"
+              , paramType = "path"
+              , defaultValue = "None"
+              , example = "1"),
+
+          @ApiImplicitParam(
+              name = "documentId"
+              , value = "문서 id"
+              , required = true
+              , dataType = "String"
+              , paramType = "path"
+              , defaultValue = "None"
+              , example = "657595c6c97b622e0440f394"),
+          @ApiImplicitParam(
+              name = "commentId"
+              , value = "댓글 id"
+              , required = true
+              , dataType = "String"
+              , paramType = "path"
+              , defaultValue = "None"
+              , example = "6575d6fe99101a62f9710877")
+      })
   @DeleteMapping("/{commentId}")
   public ResponseEntity<DeleteCommentsResponse> deleteComment(
       @PathVariable Long teamId, @PathVariable String documentId,
