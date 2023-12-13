d1cb39fb5fadaf3c2a3e4a8eeb99342f364d3c9f
정도헌 <89259535+bslkuye@users.noreply.github.com>
2023-12-13 12:16:01 +0900

    Feat : 댓글 뷰페이지

---
+++ frontend/src/views/CommentView.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Comment from "../components/documentEditor/Comment";
+
+interface CommentViewProps {}
+
+const CommentView: React.FC<CommentViewProps> = () => {
+  return <Comment />;
+};
+
+export default CommentView;
