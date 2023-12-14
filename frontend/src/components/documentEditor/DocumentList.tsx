6b615d4fe1b65d7d0c97ac25b94a707489401d11
정도헌 <89259535+bslkuye@users.noreply.github.com>
2023-12-14 14:41:15 +0900

    Add : 검색 기능 로직 추가

--- frontend/src/components/documentEditor/DocumentList.tsx
+++ frontend/src/components/documentEditor/DocumentList.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { useRecoilValue } from "recoil";
+import { accessTokenState } from "./atoms";
 
-// 스타일 컴포넌트 정의
 const DocumentContainer = styled.div`
   box-sizing: border-box;
   width: 1024px;
@@ -42,6 +44,19 @@ const DatesContainer = styled.div`
   flex-direction: column;
   align-items: flex-end;
   min-width: 150px;
+`;
+
+const Container = styled.section`
+  min-height: 800px;
+`;
+
+const SearchInput = styled.input`
+  background-color: white;
+  width: 50%;
+  height: 28px;
+  color: black;
+  font-size: 16px;
+  border-radius: 8px;
 `;
 
 // 타입 정의
@@ -272,28 +287,42 @@ const testdocument = [
 // 컴포넌트
 const DocumentList: React.FC<DocumentListProps> = ({ teamId }) => {
   const [documents, setDocuments] = useState<Document[]>(testdocument);
+  const [filteredDocuments, setFilteredDocuments] = useState<Document[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
   const [currentPage, setCurrentPage] = useState(0);
   const pageSize = 10;
+  const accessToken = useRecoilValue(accessTokenState);
 
   useEffect(() => {
     const url = `/team/${teamId}/documents?page=${currentPage}&size=10`;
     fetch(url)
       .then((response) => response.json())
-      .then((data) => setDocuments(data))
+      .then((data) => {
+        setDocuments(data);
+        setFilteredDocuments(data);
+      })
       .catch((error) => console.error("Error fetching data: ", error));
-  }, []);
+  }, [currentPage, teamId]);
 
-  const totalPages = Math.ceil(documents.length / pageSize);
-  const currentDocuments = documents.slice(
-    currentPage * pageSize,
-    (currentPage + 1) * pageSize,
-  );
+  useEffect(() => {
+    const filtered = documents.filter(
+      (doc) =>
+        doc.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        doc.content.toLowerCase().includes(searchTerm.toLowerCase()),
+    );
+    setFilteredDocuments(filtered);
+  }, [searchTerm, documents]);
 
-  const handlePageChange = (page) => {
+  const handlePageChange = (page: React.SetStateAction<number>) => {
     setCurrentPage(page);
   };
 
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
   const renderPagination = () => {
+    const totalPages = Math.ceil(filteredDocuments.length / pageSize);
     let pages = [];
     for (let i = 0; i < totalPages; i++) {
       pages.push(
@@ -305,12 +334,58 @@ const DocumentList: React.FC<DocumentListProps> = ({ teamId }) => {
     return <div>{pages}</div>;
   };
 
+  const currentDocuments = filteredDocuments.slice(
+    currentPage * pageSize,
+    (currentPage + 1) * pageSize,
+  );
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchDocuments = async () => {
+      try {
+        const response = await fetch(
+          `/team/${teamId}/documents?page=${currentPage}&size=10`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          },
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setDocuments(data);
+        setFilteredDocuments(data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchDocuments();
+  }, [currentPage, teamId]);
+
+  // 문서 클릭 핸들러
+  const handleDocumentClick = (documentId: string) => {
+    navigate(`/team/${teamId}/documents/${documentId}`);
+  };
+
   return (
-    <>
-      <StyledButton>Add docs</StyledButton>
+    <Container>
+      <SearchInput
+        type="text"
+        placeholder="제목, 내용 검색"
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <StyledButton>문서 작성</StyledButton>
       <DocumentContainer>
         {currentDocuments.map((doc) => (
-          <DocumentItem key={doc.documentId}>
+          <DocumentItem
+            key={doc.documentId}
+            onClick={() => handleDocumentClick(doc.documentId)}
+          >
             <TitleContentContainer>
               <h2>{doc.title}</h2>
               <p>{doc.content}</p>
@@ -323,7 +398,7 @@ const DocumentList: React.FC<DocumentListProps> = ({ teamId }) => {
         ))}
       </DocumentContainer>
       {renderPagination()}
-    </>
+    </Container>
   );
 };
 
