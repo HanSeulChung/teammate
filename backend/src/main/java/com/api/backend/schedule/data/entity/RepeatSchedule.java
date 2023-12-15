ce6d43aad9340b0a84305bb346031a615b468dd0
zenxxn <bunbun2438@naver.com>
2023-12-15 09:57:08 +0900

    Add: 반복일정 수정시에 필요한 originRepeatScheduleId 필드 추가

--- backend/src/main/java/com/api/backend/schedule/data/entity/RepeatSchedule.java
+++ backend/src/main/java/com/api/backend/schedule/data/entity/RepeatSchedule.java
@@ -56,6 +56,8 @@ public class RepeatSchedule extends BaseEntity {
   private String dayOfWeek;
   @Setter
   private String color;
+  @Setter
+  private Long originRepeatScheduleId;
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "team_id")
