fa0d0ed9077c37608cc5aab9af43576ded6de981
zenxxn <bunbun2438@naver.com>
2023-12-15 16:28:56 +0900

    Add: 반복일정수정시 카테고리 변경을 위한 @Setter 적용

--- backend/src/main/java/com/api/backend/schedule/data/entity/RepeatSchedule.java
+++ backend/src/main/java/com/api/backend/schedule/data/entity/RepeatSchedule.java
@@ -65,6 +65,7 @@ public class RepeatSchedule extends BaseEntity {
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "schedule_category_id")
+  @Setter
   private ScheduleCategory scheduleCategory;
 
   @Setter
