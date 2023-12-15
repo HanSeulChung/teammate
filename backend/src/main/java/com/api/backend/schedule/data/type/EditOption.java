b8c31c424671a4c343e104f11e9a0560bbc34686
zenxxn <bunbun2438@naver.com>
2023-12-15 09:54:47 +0900

    Style: 가독성 향상을 위해 Option enum을 EditOption으로 변경

--- backend/src/main/java/com/api/backend/schedule/data/type/EditOption.java
+++ backend/src/main/java/com/api/backend/schedule/data/type/EditOption.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum Option {
+public enum EditOption {
   THIS_SCHEDULE("이 일정"),
   PAST_AND_FUTURE_SCHEDULES("이 일정 및 향후 일정"),
   ALL_SCHEDULES("모든 일정");
