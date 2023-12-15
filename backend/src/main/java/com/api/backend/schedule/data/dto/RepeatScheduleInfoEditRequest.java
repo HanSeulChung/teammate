f404b868f456ca8cc6cc7c16342ebeea16ea4824
zenxxn <bunbun2438@naver.com>
2023-12-15 18:32:12 +0900

    Add: 일정 수정시 상황별 필요한 dto 생성

--- backend/src/main/java/com/api/backend/schedule/data/dto/RepeatScheduleInfoEditRequest.java
+++ backend/src/main/java/com/api/backend/schedule/data/dto/RepeatScheduleInfoEditRequest.java
@@ -1,2 +1,38 @@
-package com.api.backend.schedule.data.dto;public class RepeatScheduleInfoEditRequest {
+package com.api.backend.schedule.data.dto;
+
+import com.api.backend.schedule.customValidAnnotation.StartAndEndDtCheck;
+import com.api.backend.schedule.data.type.EditOption;
+import com.api.backend.schedule.data.type.RepeatCycle;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+@StartAndEndDtCheck(scheduleStart = "startDt", scheduleEnd = "endDt")
+public class RepeatScheduleInfoEditRequest {
+  private Long repeatScheduleId;
+  private Long teamId;
+  private Long categoryId;
+  private String title;
+  private String content;
+  private String place;
+
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+  private LocalDateTime startDt;
+
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+  private LocalDateTime endDt;
+
+  private RepeatCycle repeatCycle;
+  private String month;
+  private int day;
+  private String dayOfWeek;
+  private List<Long> teamParticipantsIds;
+  private String color;
+  private EditOption editOption;
 }
--- backend/src/main/java/com/api/backend/schedule/data/dto/RepeatToSimpleScheduleEditRequest.java
+++ backend/src/main/java/com/api/backend/schedule/data/dto/RepeatToSimpleScheduleEditRequest.java
@@ -1,2 +1,35 @@
-package com.api.backend.schedule.data.dto;public class RepeatToSimpleScheduleEditRequest {
+package com.api.backend.schedule.data.dto;
+
+import com.api.backend.schedule.customValidAnnotation.StartAndEndDtCheck;
+import com.api.backend.schedule.data.type.EditOption;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+@StartAndEndDtCheck(scheduleStart = "startDt", scheduleEnd = "endDt")
+//반복일정  -> 단순일정
+public class RepeatToSimpleScheduleEditRequest {
+  private Long repeatScheduleId;
+  private Long teamId;
+  private Long categoryId;
+  private String title;
+  private String content;
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+  private LocalDateTime startDt;
+
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+  private LocalDateTime endDt;
+
+  private String place;
+  private List<Long> teamParticipantsIds;
+  private String color;
+  private EditOption editOption;
+  private Long originRepeatScheduleId;
+  private boolean isConverted;
 }
