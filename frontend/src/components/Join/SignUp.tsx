7e05586876b91f72c25278aac206c2101a82b967
zzannorita <tpals0518@naver.com>
2023-12-16 00:49:27 +0900

    Fix: 로그인, 회원가입 token 수정

--- frontend/src/components/Join/SignUp.tsx
+++ frontend/src/components/Join/SignUp.tsx
@@ -26,9 +26,7 @@ const SignUp: React.FC<SignUpProps> = () => {
 
   const handleSignUp = () => {
     if (
-      [email, password, repassword, name].some(
-        (value) => value.trim() === "",
-      )
+      [email, password, repassword, name].some((value) => value.trim() === "")
     ) {
       setSignupMessage("입력되지 않은 항목이 있습니다.");
       return;
@@ -48,30 +46,10 @@ const SignUp: React.FC<SignUpProps> = () => {
     }
     if (isIdAvailable === null) {
       setSignupMessage("아이디 중복 확인이 필요합니다.");
-    } else if (
-      isIdAvailable &&
-      isRepasswordValid &&
-      isEmailFormatValid 
-    ) {
+    } else if (isIdAvailable && isRepasswordValid && isEmailFormatValid) {
       axios
-      // .post("/sign-up", {
-      //   id: email,
-      //   password,
-      //   repassword,
-      //   name,
-      //   sex: sexType,
-      // })
-      // .then((response) => {
-      //   console.log("회원가입 성공:", response.data);
-      //   setSignupMessage("회원가입 성공");
-      //   openModal();
-      // })
-      // .catch((error) => {
-      //   console.error("회원가입 실패:", error.response.data);
-      //   setSignupMessage("회원가입 실패");
-      //   openModal();
         .post(
-          "http://localhost:8080/sign-up",
+          "http://118.67.128.124:8080/sign-up",
           {
             email: email,
             password: password,
@@ -89,10 +67,23 @@ const SignUp: React.FC<SignUpProps> = () => {
           openModal();
         })
         .catch((error) => {
-            console.error("회원가입 실패:", error.data);
-            setSignupMessage("회원가입 실패");
-        })
-        
+          console.error("회원가입 실패:", error.data);
+          if (error.response) {
+            // 서버 응답이 있을 경우
+            console.error("서버 응답 데이터:", error.response.data);
+            console.error("서버 응답 상태 코드:", error.response.status);
+            console.error("서버 응답 헤더:", error.response.headers);
+          } else if (error.request) {
+            // 요청이 전송되었지만 응답을 받지 못한 경우
+            console.error("서버 응답이 없습니다.");
+            console.error("요청 데이터:", error.request);
+          } else {
+            // 오류가 발생한 경우
+            console.error("에러 메세지:", error.message);
+          }
+
+          setSignupMessage("회원가입 실패");
+        });
     } else if (!isIdAvailable) {
       setSignupMessage("아이디가 이미 사용 중입니다.");
     }
@@ -126,18 +117,38 @@ const SignUp: React.FC<SignUpProps> = () => {
     if (isEmailFormatValid === null || !isEmailFormatValid) {
       return;
     }
-    const dummyApiCall = () =>
-      Promise.resolve({ isAvailable: email !== "test@naver.com" });
-    dummyApiCall().then((response) => {
-      setIsIdAvailable(response.isAvailable);
-    });
+    axios
+      .post(`http://118.67.128.124:8080/sign-up/email-check`, { id: email })
+      .then((response) => {
+        // 비어 있는 응답 데이터 처리
+        const isEmailAvailable =
+          response.data === "" || response.data === "사용가능한 이메일입니다.";
+        setIsIdAvailable(isEmailAvailable);
+        console.log("이메일 중복 확인 성공!");
+      })
+      .catch((error) => {
+        console.error("이메일 중복 확인 실패:", error);
+        if (error.response) {
+          // 서버 응답이 있을 경우
+          console.error("서버 응답 데이터:", error.response.data);
+          console.error("서버 응답 상태 코드:", error.response.status);
+          console.error("서버 응답 헤더:", error.response.headers);
+        } else if (error.request) {
+          // 요청이 전송되었지만 응답을 받지 못한 경우
+          console.error("서버 응답이 없습니다.");
+          console.error("요청 데이터:", error.request);
+        } else {
+          // 오류가 발생한 경우
+          console.error("에러 메세지:", error.message);
+        }
+      });
   };
   const openModal = () => {
     setIsModalOpen(true);
   };
   const handleModalConfirm = () => {
     setIsModalOpen(false);
-      navigate("/signin");
+    navigate("/signin");
   };
 
   return (
@@ -249,9 +260,7 @@ const SignUp: React.FC<SignUpProps> = () => {
       {signupMessage && <p style={{ color: "red" }}>{signupMessage}</p>}
       {isModalOpen && (
         <div className="modal">
-          <p>
-            아이디로 이메일 인증을 보냈습니다. 확인해주세요.
-          </p>
+          <p>아이디로 이메일 인증을 보냈습니다. 확인해주세요.</p>
           <button onClick={handleModalConfirm}>확인</button>
         </div>
       )}
--- frontend/src/components/Login/SignIn.tsx
+++ frontend/src/components/Login/SignIn.tsx
@@ -5,8 +5,8 @@ import { StyledContainer, StyledFormItem } from "./SignInStyled";
 import { useRecoilState } from "recoil";
 import {
   isAuthenticatedState,
-  accessTokenState,
-  refreshTokenState,
+  // accessTokenState,
+  // refreshTokenState,
   saveAccessToken,
   saveRefreshToken,
   useUser,
@@ -18,27 +18,10 @@ const SignIn = () => {
   const [error, setError] = useState("");
   const [isAuthenticated, setIsAuthenticated] =
     useRecoilState(isAuthenticatedState);
-  const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
-  const [refreshToken, setRefreshToken] = useRecoilState(refreshTokenState);
+  // const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
+  // const [refreshToken, setRefreshToken] = useRecoilState(refreshTokenState);
   const { setUser } = useUser();
   const navigate = useNavigate();
-  const token = localStorage.getItem("accessToken");
-
-  //Mock users data for testing
-  // const mockUsers = [
-  //   { id: "user1@example.com", password: "password1", name: "김팀장" },
-  //   { id: "user2@example.com", password: "password2", name: "이팀원" },
-  // ];
-
-  const handleLogout = () => {
-    //실제론 여기 지워야함
-    // localStorage.removeItem("accessToken");
-    // localStorage.removeItem("refreshToken");
-    saveAccessToken("");
-    saveRefreshToken("");
-    navigate("/signin");
-    setIsAuthenticated(false);
-  };
 
   const handleSignIn = async () => {
     if (!email.trim() || !password.trim()) {
@@ -58,7 +41,7 @@ const SignIn = () => {
         return;
       }
       const response = await axios.post(
-        "http://localhost:8080/sign-in",
+        "http://118.67.128.124:8080/sign-in",
         {
           email: email,
           password: password,
@@ -67,29 +50,28 @@ const SignIn = () => {
           withCredentials: true,
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`, // JWT 토큰을 Authorization 헤더에 포함
           },
         },
       );
 
-      const newAccessToken = response.data.accessToken;
-      const newRefreshToken = response.data.refreshToken;
+      const { token } = response.data;
+      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      // const newAccessToken = response.data.accessToken;
+      // const newRefreshToken = response.data.refreshToken;
 
-      if (newAccessToken && newRefreshToken) {
-        // 토큰 저장
-        saveAccessToken(newAccessToken);
-        saveRefreshToken(newRefreshToken);
+      // if (newAccessToken && newRefreshToken) {
+      //   // 토큰 저장
+      //   saveAccessToken(newAccessToken);
+      //   saveRefreshToken(newRefreshToken);
 
-        // Recoil 상태 업데이트
-        setAccessToken(newAccessToken);
-        setRefreshToken(newRefreshToken);
+      //   // Recoil 상태 업데이트
+      //   setAccessToken(newAccessToken);
+      //   setRefreshToken(newRefreshToken);
 
-        setIsAuthenticated(true);
-        setUser({ id: email, name: response.data.name });
-        navigate("/homeview");
-      } else {
-        setError("토큰이 올바르게 전달되지 않았습니다.");
-      }
+      setIsAuthenticated(true);
+      setUser({ id: email, name: response.data.name });
+      navigate("/homeview");
+      console.log("login successful");
     } catch (error) {
       console.error("Sign In Error:", error);
 
@@ -106,35 +88,15 @@ const SignIn = () => {
     }
   };
 
-  //연습
-  // const user = mockUsers.find(
-  //   (u) => u.id === email && u.password === password,
-  //   );
-  //   if (user) {
-  //     const accessToken = "가짜AccessToken";
-  //     saveAccessToken(accessToken);
-  //     // setIsAuthenticated(true);
-  //     // setUser({ id: email, name: user.name });
-  //     // 사용자 ID를 기반으로 사용자 팀 정보를 가져오거나 초기화
-  //   const userTeams = JSON.parse(localStorage.getItem(user.id) || '[]') as any;
-
-  //   // 사용자가 생성한 팀 정보를 추가
-  //   const newTeam = { id: "팀ID", name: "팀 이름", size: "팀 크기", image: "이미지" };
-  //   userTeams.push(newTeam);
-
-  //   // 사용자 ID를 기반으로 업데이트된 팀 정보를 저장
-  //   localStorage.setItem(user.id, JSON.stringify(userTeams));
-
-  //   setIsAuthenticated(true);
-  //   setUser({ id: email, name: user.name });
-  //     navigate("/homeview");
-  //   } else {
-  //     setError("올바른 이메일 또는 비밀번호를 입력하세요.");
-  //   }
-  // } catch (error) {
-  //   console.error(error);
-  // }
-  // };
+  const handleLogout = () => {
+    //실제론 여기 지워야함
+    // localStorage.removeItem("accessToken");
+    // localStorage.removeItem("refreshToken");
+    saveAccessToken("");
+    saveRefreshToken("");
+    navigate("/signin");
+    setIsAuthenticated(false);
+  };
 
   const handleSignUp = () => {
     navigate("/signup");
