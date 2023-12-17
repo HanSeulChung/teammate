8b43634c2203dfeada9ca828c933c4bf06463eb2
zzannorita <tpals0518@naver.com>
2023-12-18 02:00:29 +0900

    Fix: 카카오 로그인 base_url 배포 url로 변경

--- frontend/src/components/Login/Kakao.tsx
+++ frontend/src/components/Login/Kakao.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 const Kakao = () => {
-  const REST_API_KEY = import.meta.env.VITE_FAKE_STORE_API;
-  const REDIRECT_URI = "http://localhost:3000/kakaoLogin";
+  const REST_API_KEY = import.meta.env.VITE_REST_API;
+  const REDIRECT_URI = "http://118.67.128.124:8080/kakaoLogin";
   const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   const loginHandler = () => {
