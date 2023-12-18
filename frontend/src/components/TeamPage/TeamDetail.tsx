354ab8058ad0190a9f97a5b8e32ec478825d8ef6
zzannorita <tpals0518@naver.com>
2023-12-18 12:05:27 +0900

    Fix: 팀 id 받아오게 팀 디테일페이지 변경

--- frontend/src/components/TeamPage/TeamDetail.tsx
+++ frontend/src/components/TeamPage/TeamDetail.tsx
@@ -1,21 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axiosInstance from "../../axios";
 import styled from "styled-components";
 import { useParams } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { teamListState, userState } from "../../state/authState";
 
-const TeamDetail = () => {
-  const { teamId } = useParams();
-  const teamList = useRecoilValue(teamListState);
-  const team = teamList.find((team) => team.id === teamId);
+interface TeamDetailResponse {
+  teamId: number;
+  teamName: string;
+  code: string;
+  memberLimit: number;
+  inviteLink: string;
+  teamImg: string;
+}
 
-  if (!team) {
-    return <div>팀을 찾을 수 없습니다.</div>;
+const TeamDetail = () => {
+  const { teamId } = useParams<{ teamId: string }>();
+  console.log("Team ID:", teamId);
+  const [team, setTeam] = useState<TeamDetailResponse | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const teamList = useRecoilValue(teamListState);
+
+  useEffect(() => {
+    const fetchTeamDetail = async () => {
+      try {
+        if (teamId) {
+          const response = await axiosInstance.get<TeamDetailResponse>(
+            `/team/${teamId}`,
+          );
+          setTeam(response.data);
+        } else {
+          // teamId가 없는 경우에 대한 처리
+          console.error("팀 ID가 없습니다.");
+          setError("팀 ID가 없습니다.");
+        }
+      } catch (error) {
+        console.error("Error fetching team detail:", error);
+        setError("팀 정보를 가져오는 중에 오류가 발생했습니다.");
+      }
+    };
+
+    fetchTeamDetail();
+  }, [teamId]);
+
+  if (error) {
+    return <div>Error: {error}</div>;
   }
 
   return (
     <TeamDetailContainer>
       {/* <TeamName>{team.name}</TeamName> */}
+      <h1>{team?.teamName}</h1>
       <BoxContainer>
         <EmptyBox>공유문서리스트</EmptyBox>
         <EmptyBox>캘린더</EmptyBox>
@@ -44,4 +79,3 @@ const BoxContainer = styled.div`
   display: flex;
   margin-top: 20px;
 `;
-
