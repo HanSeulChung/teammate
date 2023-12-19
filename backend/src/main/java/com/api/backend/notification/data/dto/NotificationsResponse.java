616fbb427351005569d1655995d63ae34783d2e2
jin young lee <dkwktm45@naver.com>
2023-12-19 16:01:42 +0900

    Style: 변수명 변경

--- backend/src/main/java/com/api/backend/notification/data/dto/NotificationsResponse.java
+++ backend/src/main/java/com/api/backend/notification/data/dto/NotificationsResponse.java
@@ -1,6 +1,7 @@
 package com.api.backend.notification.data.dto;
 
 import com.api.backend.notification.data.entity.Notification;
+import com.api.backend.notification.data.type.AlarmType;
 import com.api.backend.notification.data.type.Type;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
@@ -16,7 +17,7 @@ import org.springframework.data.domain.Page;
 public class NotificationsResponse {
 
   private Long notificationId;
-  private Type type;
+  private AlarmType alarmType;
   private String teamName;
   private String nickName;
   private String message;
@@ -27,7 +28,7 @@ public class NotificationsResponse {
     return NotificationsResponse.builder()
         .notificationId(notification.getNotificationId())
         .message(notification.getMessage())
-        .type(notification.getType())
+        .alarmType(notification.getAlarmType())
         .createDt(notification.getCreateDt())
         .teamName(notification.getTeamName())
         .nickName(notification.getNickName())
