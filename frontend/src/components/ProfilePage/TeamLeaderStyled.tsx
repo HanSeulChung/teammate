8838da09fb4914b6a6f5e165050fc575b87667a0
zzannorita <tpals0518@naver.com>
2023-12-20 01:06:08 +0900

    Refactor: 팀 리더 페이지 스타일 수정

--- frontend/src/components/ProfilePage/TeamLeaderStyled.tsx
+++ frontend/src/components/ProfilePage/TeamLeaderStyled.tsx
@@ -11,10 +11,83 @@ export const CenteredContainer = styled.div`
     padding: 20px;
     background: #cccccc;
   }
+  input,
+  select {
+    width: 100px;
+    background-color: #cccccc;
+    border: 1px solid white;
+    padding: 6px;
+    margin-bottom: 8px;
+  }
 `;
 
 export const TeamLeaderModal = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+`;
+
+export const StyledButton = styled.button`
+  margin-top: 10px;
+  padding: 8px 16px;
+  background-color: #a3cca3;
+  color: white;
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+  color: #333333;
+
+  &:hover {
+    background-color: #cccccc;
+  }
+`;
+
+export const TeamLeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+export const TeamProfileSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const TeamImageContainer = styled.div`
+  /* Add styles as needed */
+`;
+
+export const TeamInfoContainer = styled.div`
+  input,
+  select {
+    width: 100px;
+    background-color: white;
+    border: 1px solid #cccccc;
+    border-radius: 10px;
+  }
+`;
+
+export const StyledInput = styled.input`
+  width: 75%;
+  background-color: white;
+  border: 1px solid #cccccc;
+  border-radius: 10px;
+  padding: 6px;
+  margin-right: 5px;
+`;
+
+export const TeamMembersContainer = styled.div`
+  width: 300px;
+`;
+
+export const ConfirmationModal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 20px;
+  background: white;
+  z-index: 999;
 `;
