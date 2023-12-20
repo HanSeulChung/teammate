7537d050fb42add78d449c67a9a5e1337b6243c3
zenxxn <bunbun2438@naver.com>
2023-12-21 00:55:10 +0900

    Add: 일정 정보 수정시 일정 유형별 Edit Response DTO 생성

---
+++ backend/src/main/java/com/api/backend/schedule/data/dto/RepeatScheduleInfoEditResponse.java
@@ -0,0 +1,94 @@
+package com.api.backend.schedule.data.dto;
+
+import com.api.backend.schedule.data.entity.RepeatSchedule;
+import com.api.backend.schedule.data.entity.TeamParticipantsSchedule;
+import com.api.backend.schedule.data.type.RepeatCycle;
+import com.api.backend.team.data.type.TeamRole;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class RepeatScheduleInfoEditResponse {
+  private Long scheduleId;
+  private Long originRepeatScheduleId;
+  private String scheduleType;
+  private String categoryName;
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+  private LocalDateTime startDt;
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+  private LocalDateTime endDt;
+  private String title;
+  private String content;
+  private String place;
+  private String color;
+  private RepeatCycle repeatCycle;
+  private String month;
+  private int day;
+  private String dayOfWeek;
+  private List<Long> teamParticipantsIds;
+  private List<String> teamParticipantsNames;
+  private List<TeamRole> teamRoles;
+
+  public static RepeatScheduleInfoEditResponse from(RepeatSchedule repeatSchedule) {
+    return RepeatScheduleInfoEditResponse.builder()
+        .scheduleId(repeatSchedule.getRepeatScheduleId())
+        .originRepeatScheduleId(repeatSchedule.getOriginRepeatScheduleId())
+        .scheduleType("반복 일정")
+        .categoryName(repeatSchedule.getScheduleCategory().getCategoryName())
+        .startDt(repeatSchedule.getStartDt())
+        .endDt(repeatSchedule.getEndDt())
+        .title(repeatSchedule.getTitle())
+        .content(repeatSchedule.getContent())
+        .place(repeatSchedule.getPlace())
+        .color(repeatSchedule.getColor())
+        .repeatCycle(repeatSchedule.getRepeatCycle())
+        .month(repeatSchedule.getMonth())
+        .day(repeatSchedule.getDay())
+        .dayOfWeek(repeatSchedule.getDayOfWeek())
+        .teamParticipantsIds(getTeamParticipantsIdsFromSchedules(repeatSchedule.getTeamParticipantsSchedules()))
+        .teamParticipantsNames(getTeamParticipantsNameFromSchedules(repeatSchedule.getTeamParticipantsSchedules()))
+        .teamRoles(getTeamParticipantsRoleFromSchedules(repeatSchedule.getTeamParticipantsSchedules()))
+        .build();
+  }
+
+  public static List<Long> getTeamParticipantsIdsFromSchedules(
+      List<TeamParticipantsSchedule> teamParticipantsSchedules) {
+    List<Long> teamParticipantsIds = new ArrayList<>();
+    if (teamParticipantsSchedules != null) {
+      for (TeamParticipantsSchedule teamParticipantsSchedule : teamParticipantsSchedules) {
+        teamParticipantsIds.add(
+            teamParticipantsSchedule.getTeamParticipants().getTeamParticipantsId());
+      }
+    }
+    return teamParticipantsIds;
+  }
+
+  public static List<String> getTeamParticipantsNameFromSchedules(
+      List<TeamParticipantsSchedule> teamParticipantsSchedules) {
+    List<String> teamParticipantsNames = new ArrayList<>();
+    if (teamParticipantsSchedules != null) {
+      for (TeamParticipantsSchedule teamParticipantsSchedule : teamParticipantsSchedules) {
+        teamParticipantsNames.add(teamParticipantsSchedule.getTeamParticipants().getTeamNickName());
+      }
+    }
+    return teamParticipantsNames;
+  }
+
+  public static List<TeamRole> getTeamParticipantsRoleFromSchedules(
+      List<TeamParticipantsSchedule> teamParticipantsSchedules) {
+    List<TeamRole> teamParticipantsRoles = new ArrayList<>();
+    if (teamParticipantsSchedules != null) {
+      for (TeamParticipantsSchedule teamParticipantsSchedule : teamParticipantsSchedules) {
+        teamParticipantsRoles.add(teamParticipantsSchedule.getTeamParticipants().getTeamRole());
+      }
+    }
+    return teamParticipantsRoles;
+  }
+}
---
+++ backend/src/main/java/com/api/backend/schedule/data/dto/SimpleScheduleInfoEditResponse.java
@@ -0,0 +1,87 @@
+package com.api.backend.schedule.data.dto;
+
+import com.api.backend.schedule.data.entity.SimpleSchedule;
+import com.api.backend.schedule.data.entity.TeamParticipantsSchedule;
+import com.api.backend.team.data.type.TeamRole;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class SimpleScheduleInfoEditResponse {
+
+  private Long scheduleId;
+  private String scheduleType;
+  private String categoryName;
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+  private LocalDateTime startDt;
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+  private LocalDateTime endDt;
+  private String title;
+  private String content;
+  private String place;
+  private String color;
+  private List<Long> teamParticipantsIds;
+  private List<String> teamParticipantsNames;
+  private List<TeamRole> teamRoles;
+
+  public static SimpleScheduleInfoEditResponse from(SimpleSchedule simpleSchedule) {
+    return SimpleScheduleInfoEditResponse.builder()
+        .scheduleId(simpleSchedule.getSimpleScheduleId())
+        .scheduleType("단순 일정")
+        .categoryName(simpleSchedule.getScheduleCategory().getCategoryName())
+        .startDt(simpleSchedule.getStartDt())
+        .endDt(simpleSchedule.getEndDt())
+        .title(simpleSchedule.getTitle())
+        .content(simpleSchedule.getContent())
+        .place(simpleSchedule.getPlace())
+        .color(simpleSchedule.getColor())
+        .teamParticipantsIds(
+            getTeamParticipantsIdsFromSchedules(simpleSchedule.getTeamParticipantsSchedules()))
+        .teamParticipantsNames(
+            getTeamParticipantsNameFromSchedules(simpleSchedule.getTeamParticipantsSchedules()))
+        .teamRoles(
+            getTeamParticipantsRoleFromSchedules(simpleSchedule.getTeamParticipantsSchedules()))
+        .build();
+  }
+
+  public static List<Long> getTeamParticipantsIdsFromSchedules(
+      List<TeamParticipantsSchedule> teamParticipantsSchedules) {
+    List<Long> teamParticipantsIds = new ArrayList<>();
+    if (teamParticipantsSchedules != null) {
+      for (TeamParticipantsSchedule teamParticipantsSchedule : teamParticipantsSchedules) {
+        teamParticipantsIds.add(
+            teamParticipantsSchedule.getTeamParticipants().getTeamParticipantsId());
+      }
+    }
+    return teamParticipantsIds;
+  }
+
+  public static List<String> getTeamParticipantsNameFromSchedules(
+      List<TeamParticipantsSchedule> teamParticipantsSchedules) {
+    List<String> teamParticipantsNames = new ArrayList<>();
+    if (teamParticipantsSchedules != null) {
+      for (TeamParticipantsSchedule teamParticipantsSchedule : teamParticipantsSchedules) {
+        teamParticipantsNames.add(teamParticipantsSchedule.getTeamParticipants().getTeamNickName());
+      }
+    }
+    return teamParticipantsNames;
+  }
+
+  public static List<TeamRole> getTeamParticipantsRoleFromSchedules(
+      List<TeamParticipantsSchedule> teamParticipantsSchedules) {
+    List<TeamRole> teamParticipantsRoles = new ArrayList<>();
+    if (teamParticipantsSchedules != null) {
+      for (TeamParticipantsSchedule teamParticipantsSchedule : teamParticipantsSchedules) {
+        teamParticipantsRoles.add(teamParticipantsSchedule.getTeamParticipants().getTeamRole());
+      }
+    }
+    return teamParticipantsRoles;
+  }
+}
