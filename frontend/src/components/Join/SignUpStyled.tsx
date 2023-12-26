1355af3af4953a3523618f1e7538980d0f9bc6cd
zzannorita <tpals0518@naver.com>
2023-12-27 01:53:36 +0900

    Refactor: 회원가입 에러 문구 위치 변경

--- frontend/src/components/Join/SignUpStyled.tsx
+++ frontend/src/components/Join/SignUpStyled.tsx
@@ -3,13 +3,14 @@ import styled from "styled-components";
 export const StyledContainer = styled.div`
   color: #333333;
   padding: 20px;
-  max-width: 300px;
+  max-width: 400px;
   margin: auto;
 `;
 
 export const StyledFormItem = styled.div`
   margin: 5px 0 5px 0;
   display: flex;
+  width: 350px;
 
   label {
     margin-right: 60px;
@@ -40,17 +41,19 @@ export const StyledFormItem = styled.div`
       background-color: #cccccc;
     }
   }
+  span {
+    margin-left: 30px;
+  }
 `;
 
-export const Button = styled.div`
+export const Button = styled.button`
   button {
     flex: 1;
-    margin-left: 10px;
     padding: 8px;
     background-color: #a3cca3;
     color: #333333;
     cursor: pointer;
-
+    textAlign: "center",
     &:hover {
       background-color: #cccccc;
     }
