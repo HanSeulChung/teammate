a0a15b1743bab38ae3e24eca0b5c762c7f40b32a
zzannorita <tpals0518@naver.com>
2023-12-27 01:51:18 +0900

    Add: 팀 복구 추가 및 페이지네이션 적용

--- frontend/src/components/Home/HomeContent.tsx
+++ frontend/src/components/Home/HomeContent.tsx
@@ -4,20 +4,23 @@ import axiosInstance from "../../axios";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import {
-  useSearchState,
+  searchState,
   userTeamsState,
   accessTokenState,
   isAuthenticatedState,
 } from "../../state/authState";
 import { Team } from "../../interface/interface.ts";
 
+const ITEMS_PER_PAGE = 10;
+
 const HomeContent = () => {
   const [userTeams, setUserTeams] = useRecoilState<Team[]>(userTeamsState);
-  const { search } = useSearchState();
+  const search = useRecoilValue(searchState);
   const [error, setError] = useState<string | null>(null);
   const accessToken = useRecoilValue(accessTokenState);
   const navigate = useNavigate();
   const isAuthenticated = useRecoilValue(isAuthenticatedState);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     const fetchLoggedInUserId = async () => {
@@ -27,9 +30,7 @@ const HomeContent = () => {
           return;
         }
         const teamListResponse = await axiosInstance.get("/team/list");
-        // Recoil 상태 업데이트
         setUserTeams(teamListResponse.data);
-        // localStorage에 저장
         window.sessionStorage.setItem(
           `teamList_${accessToken}`,
           JSON.stringify(teamListResponse.data),
@@ -52,28 +53,72 @@ const HomeContent = () => {
     return null;
   }
 
-  const filteredTeamList = userTeams.filter((team) =>
-    team.name.toLowerCase().includes(search.toLowerCase()),
+  const filteredTeamList = userTeams
+    .filter((team) => team.name.toLowerCase().includes(search.toLowerCase()))
+    .filter((team) => {
+      return (
+        team.restorationDt === null ||
+        team.teamRole === "READER" ||
+        (team.teamRole === "MATE" && team.restorationDt === null)
+      );
+    });
+
+  const indexOfLastTeam = currentPage * ITEMS_PER_PAGE;
+  const indexOfFirstTeam = indexOfLastTeam - ITEMS_PER_PAGE;
+  const currentTeams = filteredTeamList.slice(
+    indexOfFirstTeam,
+    indexOfLastTeam,
   );
+
+  const paginate = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const handleRestoration = async (teamId: number) => {
+    try {
+      await axiosInstance.patch(`/team/{teamId}/restore?restoreDt=2023-01-01`);
+      const updatedTeamList = await axiosInstance.get("/team/list");
+      setUserTeams(updatedTeamList.data);
+    } catch (error: any) {
+      console.error("팀 복구 중 에러 발생:", error);
+    }
+  };
 
   return (
     <TeamListContainer>
-      {filteredTeamList.map((team, index) => (
+      {currentTeams.map((team, index) => (
         <TeamItem key={index}>
           <TeamCard
-            onClick={() =>
+            onClick={() => {
+              if (team.restorationDt !== null) {
+                return;
+              }
               navigate(`/team/${team.teamId}`, {
                 state: { team },
-              })
-            }
+              });
+            }}
           >
             <TeamName>{team.name}</TeamName>
             {team.profileUrl && (
               <TeamImage src={team.profileUrl} alt={`${team.name} 이미지`} />
             )}
+            {team.restorationDt !== null && (
+              <RestorationButton onClick={() => handleRestoration(team.teamId)}>
+                복구
+              </RestorationButton>
+            )}
           </TeamCard>
         </TeamItem>
       ))}
+      <Pagination>
+        {Array.from({
+          length: Math.ceil(filteredTeamList.length / ITEMS_PER_PAGE),
+        }).map((_, index) => (
+          <PageNumber key={index} onClick={() => paginate(index + 1)}>
+            {index + 1}
+          </PageNumber>
+        ))}
+      </Pagination>
     </TeamListContainer>
   );
 };
@@ -122,4 +167,39 @@ const TeamImage = styled.img`
   margin-bottom: 8px;
   margin: 0 auto;
   object-fit: cover;
+`;
+
+const Pagination = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 30px;
+`;
+
+const PageNumber = styled.div`
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+  color: #333333;
+  border-radius: 50px;
+  text-align: center;
+  padding: 4px;
+
+  &:hover {
+    background-color: #a3cca3;
+  }
+`;
+
+const RestorationButton = styled.button`
+  background-color: #a3cca3;
+  color: white;
+  border: none;
+  padding: 6px 12px;
+  border-radius: 4px;
+  margin-top: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #cccccc;
+  }
 `;
