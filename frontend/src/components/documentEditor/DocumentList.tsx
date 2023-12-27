14c5d64df3298e9dac3cf28aaef629da9ffb0d3b
정도헌 <89259535+bslkuye@users.noreply.github.com>
2023-12-27 14:00:54 +0900

    Fix : html 문법 문서 목록 내용에서 보이는 오류 해결

--- frontend/src/components/documentEditor/DocumentList.tsx
+++ frontend/src/components/documentEditor/DocumentList.tsx
@@ -192,7 +192,11 @@ const DocumentList: React.FC<DocumentListProps> = ({ teamId }) => {
                   <BlurLayer $blur={0.4} $width="40px" />
                 </TitleDomStyled>
                 <ContentDomStyled>
-                  내용 : {doc.content}
+                  내용 :{" "}
+                  {doc.content
+                    .replace(/<p>/g, "")
+                    .replace(/<\/p>/g, "")
+                    .replace(/<br>/g, "\n")}
                   <BlurLayer $blur={3.6} $width="5px" />
                   <BlurLayer $blur={3.2} $width="10px" />
                   <BlurLayer $blur={2.8} $width="15px" />
