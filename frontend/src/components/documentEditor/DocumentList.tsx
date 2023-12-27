eb5f10bedbecd07f380827d4588eb2de521be16e
정도헌 <89259535+bslkuye@users.noreply.github.com>
2023-12-27 16:44:06 +0900

    Refactor : 스타일 수정

--- frontend/src/components/documentEditor/DocumentList.tsx
+++ frontend/src/components/documentEditor/DocumentList.tsx
@@ -282,7 +282,7 @@ const DatesContainer = styled.div`
 
 const Container = styled.section`
   width: 800px;
-  min-height: 800px;
+  min-height: 700px;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
