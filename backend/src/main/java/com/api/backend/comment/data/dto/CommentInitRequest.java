bc3ce2306b8bbdff0d17b1995ddbdcfd5f316bfa
HanSeul <w0w1278@naver.com>
2023-12-27 23:38:42 +0900

    Refactor: 명확한 log 기록을 위해 @ToString 추가

--- backend/src/main/java/com/api/backend/comment/data/dto/CommentInitRequest.java
+++ backend/src/main/java/com/api/backend/comment/data/dto/CommentInitRequest.java
@@ -7,9 +7,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Builder
 @Getter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommentInitRequest {
