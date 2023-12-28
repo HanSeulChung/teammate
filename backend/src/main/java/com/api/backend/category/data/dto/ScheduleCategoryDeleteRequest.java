246374c4289d8a7af718eb0a21dc5c6dacece97f
zenxxn <bunbun2438@naver.com>
2023-12-29 01:23:56 +0900

    Add: request DTO에 @NotNull, @NotBlank, @Schema, @ToString 적용

--- backend/src/main/java/com/api/backend/category/data/dto/ScheduleCategoryDeleteRequest.java
+++ backend/src/main/java/com/api/backend/category/data/dto/ScheduleCategoryDeleteRequest.java
@@ -1,5 +1,7 @@
 package com.api.backend.category.data.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,9 +12,22 @@ import lombok.ToString;
 @ToString
 @Builder
 public class ScheduleCategoryDeleteRequest {
+  @NotNull(message = "categoryId는 필수 값입니다.")
+  @Schema(description = "category id", example = "1L")
   private Long categoryId;
+
+  @NotNull(message = "teamId는 필수 값입니다.")
+  @Schema(description = "team id", example = "1L")
   private Long teamId;
+
+  @NotNull(message = "participantId는 필수 값입니다.")
+  @Schema(description = "participant id", example = "1L")
   private Long participantId;
+
+  @NotNull(message = "isMoved는 필수 값입니다.")
+  @Schema(description = "is moved", example = "true")
   private boolean isMoved;
+
+  @Schema(description = "new category id", example = "2L")
   private Long newCategoryId;
 }
--- backend/src/main/java/com/api/backend/category/data/dto/ScheduleCategoryEditRequest.java
+++ backend/src/main/java/com/api/backend/category/data/dto/ScheduleCategoryEditRequest.java
@@ -1,19 +1,40 @@
 package com.api.backend.category.data.dto;
 
 import com.api.backend.category.type.CategoryType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
 @Builder
+@ToString
 public class ScheduleCategoryEditRequest {
 
+  @NotNull(message = "categoryId는 필수 값입니다.")
+  @Schema(description = "category id", example = "1L")
   private Long categoryId;
+
+  @NotNull(message = "updateTeamParticipantId는 필수 값입니다.")
+  @Schema(description = "update team participant id", example = "1L")
   private Long updateTeamParticipantId;
+
+  @NotNull(message = "teamId는 필수 값입니다.")
+  @Schema(description = "team id", example = "1L")
   private Long teamId;
+
+  @NotBlank(message = "카테고리명을 입력해주세요.")
+  @Schema(description = "category name", example = "회의")
   private String categoryName;
+
+  @NotBlank(message = "카테고리 유형을 선택해주세요.")
+  @Schema(description = "category type", example = "DOCUMENTS")
   private CategoryType categoryType;
+
+  @Schema(description = "color", example = "#ff0000")
   private String color;
 }
