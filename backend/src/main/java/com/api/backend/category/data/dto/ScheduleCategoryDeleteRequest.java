d2c888533f7472fdd516f1c3673620e68a108762
zenxxn <bunbun2438@naver.com>
2023-12-28 17:03:08 +0900

    Add: 일정 카테고리 삭제용 Request DTO 추가

--- backend/src/main/java/com/api/backend/category/data/dto/ScheduleCategoryDeleteRequest.java
+++ backend/src/main/java/com/api/backend/category/data/dto/ScheduleCategoryDeleteRequest.java
@@ -1,2 +1,17 @@
-package com.api.backend.category.data.dto;public class ScheduleCategoryDeleteRequest {
+package com.api.backend.category.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+@Builder
+public class ScheduleCategoryDeleteRequest {
+  private Long categoryId;
+  private Long teamId;
+  private Long participantId;
+  private boolean isMoved;
 }
