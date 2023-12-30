8918899c9ccc8b47a7076ea0db9660fdb1d3dbcf
zzannorita <tpals0518@naver.com>
2023-12-30 17:58:16 +0900

    Refactor: 알람 관련 파일 및 코드 삭제

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -5,7 +5,6 @@ import { isAuthenticatedState, userState } from "../../state/authState";
 import axios, { AxiosError } from "axios";
 import axiosInstance from "../../axios";
 import styled from "styled-components";
-import AlarmModal from "../alarm/AlarmModal";
 import logo from "../../assets/logo.png";
 
 const Header = () => {
@@ -14,7 +13,6 @@ const Header = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const [teamName] = useState("");
-  const [isModalOpen, setModalOpen] = useState(false);
   const [, setUser] = useRecoilState(userState);
   const isTeamPage = location.pathname.startsWith("/team/");
 
@@ -63,14 +61,6 @@ const Header = () => {
     }
   };
 
-  const handleNotificationClick = () => {
-    setModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalOpen(false);
-  };
-
   return (
     <header className="navbar bg-white">
       <GnbBodyDiv>
@@ -107,32 +97,6 @@ const Header = () => {
                   ) : (
                     <Link to="/myUserProfile">마이페이지</Link>
                   )}
-                </li>
-                <li>
-                  <button
-                    className="btn btn-ghost btn-circle"
-                    onClick={handleNotificationClick}
-                    aria-label="알림 보기"
-                  >
-                    <div className="indicator">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                        />
-                      </svg>
-                      <span className="badge badge-xs badge-primary indicator-item"></span>
-                    </div>
-                  </button>
-                  {isModalOpen && <AlarmModal closeModal={closeModal} />}{" "}
                 </li>
               </>
             ) : (
